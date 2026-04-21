44d710a0c1585642ba52820a52f4d9500667a0b0
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-04-21 15:26:18 -0300

    refactor: change base URL

--- frontend/src/axios/config.jsx
+++ frontend/src/axios/config.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const blogFetch = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
+  baseURL: "http://localhost:3000/posts/",
   headers: {
     "Content-Type": "application/json",
   },
